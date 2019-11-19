fbe7f76a670f26589ea7cedbb7e97230fc556cb0
HhMorgan <heshammse@gmail.com>
2019-11-19 22:22:52 +0100

    Implemented Listify and added predicates to the syntax of the rule based system

--- resolution.pl
+++ resolution.pl
@@ -1,5 +1,5 @@
 :-use_module(library(chr)).
-:-chr_constraint a/0,a/1,b/0,b/1,c/0,c/1,d/0,d/1,e/0,e/1,start/0,conflictdone/0,fire/0,id/1,history/1,match/2,match/3.
+:-chr_constraint a/0,a/1,b/0,b/1,c/0,c/1,d/0,d/1,e/0,e/1,c(X)/0,c(X)/1,start/0,conflictdone/0,fire/0,id/1,history/1,match/2,match/3.
 
 id(I), a <=> a(I), I1 is I + 1, id(I1).
 id(I), b <=> b(I), I1 is I + 1, id(I1).
@@ -7,9 +7,10 @@ id(I), c <=> c(I), I1 is I + 1, id(I1).
 id(I), d <=> d(I), I1 is I + 1, id(I1).
 id(I), e <=> e(I), I1 is I + 1, id(I1).
 
-start, a(ID1), b(ID2), c(ID3) ==> match(r1,4,[ID1,ID2,ID3]).
-start, a(ID1), b(ID2), d(ID3) ==> match(r2,7,[ID1,ID2,ID3]).
-start, e(ID1) ==> match(r3,4,[ID1]).
+start, a(ID1), b(ID2), c(ID3) ==> member(2,[1,2])| match(r1,4,[ID1,ID2,ID3]).
+start, a(ID1), b(ID2), d(ID3) ==> member(2,[1,2])| match(r2,7,[ID1,ID2,ID3]).
+start, e(ID1) ==> member(2,[1,2])| match(r3,4,[ID1]).
+start, c(X) ==>  match(r4,2,[]).
 
 start <=> conflictdone.
 history(L),conflictdone\match(R,_,IDs) <=> member((R,FIDs),L), sort(IDs,II), sort(FIDs,II) | true.
@@ -22,3 +23,4 @@ conflictdone <=> fire.
 a(ID1),b(ID2)\c(ID3),history(L),fire,match(r1,_,[ID1,ID2,ID3]) <=> print('fired r1'),nl,c,d,history([(r1,[ID1,ID2,ID3])|L]),start.
 a(ID1),b(ID2),d(ID3),history(L),fire,match(r2,_,[ID1,ID2,ID3]) <=> print('fired r2'),nl,c,e,history([(r2,[ID1,ID2,ID3])|L]),start.
 e(ID1)\history(L),fire,match(r3,_,[ID1]) <=> print('fired r3'),nl,c,d,history([(r3,[ID1])|L]),start.
+c(X),history(L),fire,match(r4,_,[]) <=> print('fired r4'),nl,true,history([(r4,[])|L]),start.
--- src/rulebasedProbablity/Main.java
+++ src/rulebasedProbablity/Main.java
@@ -6,8 +6,63 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+	public static ArrayList<String> splitSp(String terms) {
+		ArrayList<String> arguments = new ArrayList<String>();
+		int parenthsis = 0;
+		String currentTerm = "";
+		for (int i = 0; i < terms.length(); i++) {
+			char currentChar = terms.charAt(i);
+			if (currentChar == '(') {
+				parenthsis++;
+				currentTerm += currentChar;
+			} else {
+				if (currentChar == ')') {
+					parenthsis--;
+					currentTerm += currentChar;
+				} else {
+					if (currentChar == ',' && parenthsis == 0) {
+						arguments.add(currentTerm);
+						currentTerm = "";
+					} else {
+						currentTerm += currentChar;
+					}
+				}
+			}
+		}
+		arguments.add(currentTerm);
+		return arguments;
+	}
+
+	public static ArrayList<String> Listify(String statement) {
+		ArrayList<String> arguments = new ArrayList<String>();
+		String pattern = "(\\w+)(\\([a-zA-Z0-9\\(\\)]+([,][a-zA-Z0-9\\(\\)]+)*\\))*";
+		// Create a Pattern object
+		Pattern r = Pattern.compile(pattern);
+
+		// Now create matcher object.
+		Matcher m = r.matcher(statement);
+		if (m.find()) {
+			arguments.add(m.group(1));
+			String term = m.group(2);
+			if (term != null) {
+				term = term.substring(1, m.group(2).length() - 1);
+				ArrayList<String> arugements = splitSp(term);
+				for (String s : arugements) {
+					arguments.add(s);
+				}
+			}
+		} else {
+			System.out.println("NO MATCH");
+		}
+		return arguments;
+	}
+
 	public static void main(String[] args) {
 		ArrayList<Rule> program = new ArrayList<Rule>();
 		String filename = "program.txt";
@@ -37,8 +92,14 @@ public class Main {
 
 				String[] body = ruleString[1].split("pragma");
 				String[] bodyNotPrag = body[0].split("\\|");
-				String[] guard = bodyNotPrag[0].split(",");
-				String[] effect = bodyNotPrag[1].split(",");
+				String[] guard = new String[0];
+				String[] effect = new String[0];
+				if (bodyNotPrag.length == 2) {
+					guard = bodyNotPrag[0].split(",");
+					effect = bodyNotPrag[1].split(",");
+				} else {
+					effect = bodyNotPrag[0].split(",");
+				}
 
 				rule.setGuard(guard);
 				rule.setConsequences(effect);
@@ -88,24 +149,30 @@ public class Main {
 			if (r.getHead() != null) {
 				String[] head = r.getHead();
 				for (int i = 0; i < head.length; i++) {
-					if (!addedSoFar.contains(head[i])) {
-						chrProgram += "id(I), " + head[i] + " <=> " + head[i] + "(I), I1 is I + 1, id(I1).\n";
-						addedSoFar.add(head[i]);
+					if (Listify(head[i]).size() == 1) {
+						if (!addedSoFar.contains(head[i])) {
+							chrProgram += "id(I), " + head[i] + " <=> " + head[i] + "(I), I1 is I + 1, id(I1).\n";
+							addedSoFar.add(head[i]);
+						}
 					}
 				}
 			} else {
 				String[] kept = r.getKept();
 				String[] removed = r.getRemoved();
 				for (int i = 0; i < kept.length; i++) {
-					if (!addedSoFar.contains(kept[i])) {
-						chrProgram += "id(I), " + kept[i] + " <=> " + kept[i] + "(I), I1 is I + 1, id(I1).\n";
-						addedSoFar.add(kept[i]);
+					if (Listify(kept[i]).size() == 1) {
+						if (!addedSoFar.contains(kept[i])) {
+							chrProgram += "id(I), " + kept[i] + " <=> " + kept[i] + "(I), I1 is I + 1, id(I1).\n";
+							addedSoFar.add(kept[i]);
+						}
 					}
 				}
 				for (int i = 0; i < removed.length; i++) {
-					if (!addedSoFar.contains(removed[i])) {
-						chrProgram += "id(I), " + removed[i] + " <=> " + removed[i] + "(I), I1 is I + 1, id(I1).\n";
-						addedSoFar.add(removed[i]);
+					if (Listify(removed[i]).size() == 1) {
+						if (!addedSoFar.contains(removed[i])) {
+							chrProgram += "id(I), " + removed[i] + " <=> " + removed[i] + "(I), I1 is I + 1, id(I1).\n";
+							addedSoFar.add(removed[i]);
+						}
 					}
 				}
 			}
@@ -122,12 +189,20 @@ public class Main {
 				chrProgram += "start, ";
 				for (int i = 0; i < head.length; i++) {
 					if (i < head.length - 1) {
-						chrProgram += head[i] + "(ID" + literalCounter + "), ";
-						IDString += "ID" + literalCounter + ",";
-						literalCounter++;
+						if (Listify(head[i]).size() == 1) {
+							chrProgram += head[i] + "(ID" + literalCounter + "), ";
+							IDString += "ID" + literalCounter + ",";
+							literalCounter++;
+						} else {
+							chrProgram += head[i] + ", ";
+						}
 					} else {
-						chrProgram += head[i] + "(ID" + literalCounter + ")";
-						IDString += "ID" + literalCounter;
+						if (Listify(head[i]).size() == 1) {
+							chrProgram += head[i] + "(ID" + literalCounter + ")";
+							IDString += "ID" + literalCounter;
+						} else {
+							chrProgram += head[i];
+						}
 					}
 				}
 			} else {
@@ -135,22 +210,44 @@ public class Main {
 				String[] kept = r.getKept();
 				String[] removed = r.getRemoved();
 				for (int i = 0; i < kept.length; i++) {
-					chrProgram += kept[i] + "(ID" + literalCounter + "), ";
-					IDString += "ID" + literalCounter + ",";
-					literalCounter++;
-				}
-				for (int i = 0; i < removed.length; i++) {
-					if (i < removed.length - 1) {
-						chrProgram += removed[i] + "(ID" + literalCounter + "), ";
+					if (Listify(kept[i]).size() == 1) {
+						chrProgram += kept[i] + "(ID" + literalCounter + "), ";
 						IDString += "ID" + literalCounter + ",";
 						literalCounter++;
 					} else {
-						chrProgram += removed[i] + "(ID" + literalCounter + ")";
-						IDString += "ID" + literalCounter;
+						chrProgram += kept[i] + ", ";
+					}
+				}
+				for (int i = 0; i < removed.length; i++) {
+					if (i < removed.length - 1) {
+						if (Listify(removed[i]).size() == 1) {
+							chrProgram += removed[i] + "(ID" + literalCounter + "), ";
+							IDString += "ID" + literalCounter + ",";
+							literalCounter++;
+						} else {
+							chrProgram += removed[i] + ", ";
+						}
+					} else {
+						if (Listify(removed[i]).size() == 1) {
+							chrProgram += removed[i] + "(ID" + literalCounter + ")";
+							IDString += "ID" + literalCounter;
+						} else {
+							chrProgram += removed[i];
+						}
 					}
 				}
 			}
-			chrProgram += " ==> match(r" + ruleCounter + "," + r.getPragma() + ",[" + IDString + "]).\n";
+			String guard = " ";
+			if (r.getGuard() != null) {
+				for (int i = 0; i < r.getGuard().length; i++) {
+					if (i < r.getGuard().length - 1) {
+						guard += r.getGuard()[i] + ",";
+					} else {
+						guard += r.getGuard()[i] + "|";
+					}
+				}
+			}
+			chrProgram += " ==>" + guard + " match(r" + ruleCounter + "," + r.getPragma() + ",[" + IDString + "]).\n";
 			ruleCounter++;
 		}
 		chrProgram += "\n";
@@ -171,12 +268,20 @@ public class Main {
 
 				for (int i = 0; i < head.length; i++) {
 					if (i < head.length - 1) {
-						chrProgram += head[i] + "(ID" + literalCounter + "),";
-						IDString += "ID" + literalCounter + ",";
-						literalCounter++;
+						if (Listify(head[i]).size() == 1) {
+							chrProgram += head[i] + "(ID" + literalCounter + "),";
+							IDString += "ID" + literalCounter + ",";
+							literalCounter++;
+						} else {
+							chrProgram += head[i] + ", ";
+						}
 					} else {
-						chrProgram += head[i] + "(ID" + literalCounter + ")";
-						IDString += "ID" + literalCounter;
+						if (Listify(head[i]).size() == 1) {
+							chrProgram += head[i] + "(ID" + literalCounter + ")";
+							IDString += "ID" + literalCounter;
+						} else {
+							chrProgram += head[i];
+						}
 					}
 				}
 			} else {
@@ -185,26 +290,36 @@ public class Main {
 				String[] removed = r.getRemoved();
 				for (int i = 0; i < kept.length; i++) {
 					if (i < kept.length - 1) {
-						chrProgram += kept[i] + "(ID" + literalCounter + "),";
-						IDString += "ID" + literalCounter + ",";
-						literalCounter++;
+						if (Listify(kept[i]).size() == 1) {
+							chrProgram += kept[i] + "(ID" + literalCounter + "),";
+							IDString += "ID" + literalCounter + ",";
+							literalCounter++;
+						} else {
+							chrProgram += kept[i] + ", ";
+						}
 					} else {
-						chrProgram += kept[i] + "(ID" + literalCounter + ")";
-						IDString += "ID" + literalCounter + ",";
-						literalCounter++;
+						if (Listify(kept[i]).size() == 1) {
+							chrProgram += kept[i] + "(ID" + literalCounter + ")";
+							IDString += "ID" + literalCounter + ",";
+							literalCounter++;
+						} else {
+							chrProgram += kept[i];
+						}
 					}
 				}
 				chrProgram += "\\";
 				for (int i = 0; i < removed.length; i++) {
-
-					chrProgram += removed[i] + "(ID" + literalCounter + "),";
-					if (i < removed.length - 1) {
-						IDString += "ID" + literalCounter + ",";
-						literalCounter++;
+					if (Listify(removed[i]).size() == 1) {
+						chrProgram += removed[i] + "(ID" + literalCounter + "),";
+						if (i < removed.length - 1) {
+							IDString += "ID" + literalCounter + ",";
+							literalCounter++;
+						} else {
+							IDString += "ID" + literalCounter;
+						}
 					} else {
-						IDString += "ID" + literalCounter;
+						chrProgram += removed[i] + ", ";
 					}
-
 				}
 			}
 			String consequence = "";
